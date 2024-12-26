c6fb796b19418da6ac63b5e74b37b361a4c70746
Jamie Kuppens <jamie@amdrel.net>
2024-12-26 00:04:07 -0800

    Generate sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   site: process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : "http://localhost:4321",
 });
+
--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -7,6 +7,6 @@ Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
-  const sitemapURL = new URL("sitemap.xml", site);
+  const sitemapURL = new URL("sitemap-index.xml", site);
   return new Response(getRobotsTxt(sitemapURL));
 };
