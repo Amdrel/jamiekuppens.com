ceda6e9f63eec7c3b52ae2b06976e0741c17b3cb
Jamie Kuppens <jamie@amdrel.net>
2024-12-26 13:51:24 -0800

    Installed Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   site: process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : "http://localhost:4321",
@@ -13,3 +14,4 @@ export default defineConfig({
     prefetchAll: true,
   },
 });
+
