9936358578232a0a1d4366f1195071a3704734b8
Jamie Kuppens <jamie@amdrel.net>
2024-12-27 01:01:33 -0800

    Move socials to footer on mobile

---
+++ src/data.ts
@@ -0,0 +1,29 @@
+export const siteName = "Jamie Kuppens";
+
+export const siteDescription =
+  "Jamie Kuppens is a software engineer with over 10 years of experience building for the web.";
+
+export const navLinks = [
+  { url: "/experience", label: "Experience" },
+  { url: "/projects", label: "Projects" },
+  { url: "/contact", label: "Contact" },
+  { url: "/posts", label: "Blog" },
+];
+
+export const contactLinks = [
+  {
+    url: "https://github.com/amdrel",
+    label: "GitHub",
+    iconUrl: "/images/icons/github.svg",
+  },
+  {
+    url: "https://bsky.app/profile/amdrel.bsky.social",
+    label: "Bluesky",
+    iconUrl: "/images/icons/bluesky.svg",
+  },
+  {
+    url: "mailto://jamie@amdrel.net",
+    label: "jamie@amdrel.net",
+    iconUrl: "/images/icons/email.svg",
+  },
+];
