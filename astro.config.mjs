d585c5d46991be55d0883739c060e320aeed4e89
Jamie Kuppens <jamie@amdrel.net>
2024-12-29 21:49:58 -0800

    Update code example to be accurate

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,14 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind()],
+
   site: process.env.PRODUCTION_URL
     ? `https://${process.env.PRODUCTION_URL}`
     : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
       : "http://localhost:4321",
+
+  // Prefetch everything by default since every page is lightweight.
   prefetch: {
     prefetchAll: true,
   },
