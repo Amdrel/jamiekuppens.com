9a3eade2983c8aabc07ea2e67c245f3cc61f951e
Jamie Kuppens <jamie@amdrel.net>
2024-12-29 16:39:44 -0800

    Add redirect file and middleware for it

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,15 @@
+import redirects from "./redirects.json";
+
 export const config = {
   matcher: "/posts/:slug*",
 };
 
-export default function middleware(request: Request): Response {
-  return Response.redirect("https://www.jamiekuppens.com", 301);
+export default function middleware(request: Request) {
+  const url = new URL(request.url);
+
+  for (const [path, redirectUrl] of Object.entries(redirects)) {
+    if (url.pathname.startsWith(path)) {
+      return Response.redirect(redirectUrl, 301);
+    }
+  }
 }
