932c849486095a7a98c2affc5526be2f41cd0ade
Jamie Kuppens <jamie@amdrel.net>
2024-12-29 17:37:27 -0800

    Add CDN cache headers

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,10 @@ export const config = {
   matcher: "/posts/:slug*",
 };
 
+/**
+ * This middleware handles dynamic redirects. These Redirects are generated at
+ * build time and are placed into a 'redirects.json' file.
+ */
 export default function middleware(request: Request) {
   const url = new URL(request.url);
 
